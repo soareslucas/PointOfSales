292515d1e67853a950b461cab4a3de4656bb44c1
Lucas Soares <lucas.soarod@gmail.com>
2019-01-16 23:20:22 -0200

    First commit.

---
+++ src/main/java/br/com/goinf/controller/ProductController.java
@@ -0,0 +1,46 @@
+package br.com.goinf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+
+import br.com.goinf.entities.Product;
+
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+	
+
+	
+@RequestMapping("new")
+public String form() {
+return "product/new";
+}
+
+
+ @RequestMapping("add")
+ public ModelAndView add(Product product) {
+	 
+     ModelAndView mv = null;
+     if(product.getDescription() == null || product.getDescription().equals("")) {
+	     mv = new ModelAndView("product/new");
+     } else{
+	     mv = new ModelAndView("product/added");
+	     mv.addObject("product", product);
+     }
+     return mv;
+
+ }
+ 
+ @RequestMapping("added")
+ public String result() {
+ return "product/added";
+ }
+
+ 
+ 
+ 
+	 
+
+}
---
+++ src/main/webapp/static/product/new.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+	<body>
+	<h3>Adicionar Produtos</h3>
+		<form action="add" method="post">
+			Nome: <br />
+			<textarea name="description" rows="5" cols="100"></textarea><br />
+			<input type="submit" value="Adicionar">
+		</form>
+	</body>
+</html>
